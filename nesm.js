4ef99396ee08c3d74cb7c57c04ec94005395e903
juniper wilde <junewilde@pm.me>
2022-11-10 02:13:59 -0800

    add instruction set tables and command line flags

--- nesm.js
+++ nesm.js
@@ -8,10 +8,14 @@ import * as path from "path";
 import { readFile } from "fs/promises";
 import { Command, Argument } from "commander";
 import { extensions } from "./lib/constants.js";
+import {
+  base_instruction_set,
+  pseudo_instruction_set,
+} from "./lib/instructions_base.js";
+import * as nes from "./lib/nes.js";
 
 async function main() {
-  // GLOBAL VALUES
-  // Read in package metadata for referencing in various commands
+  // read in package metadata for referencing in various commands
   const package_json = JSON.parse(
     await readFile(new URL("package.json", import.meta.url))
   );
@@ -19,7 +23,7 @@ async function main() {
   // assembly file to be translated
   let input_file = "";
 
-  // Process command line arguments
+  // process command line arguments
   const program = new Command()
     .name(package_json.name)
     .description(
@@ -29,8 +33,11 @@ async function main() {
       `Neatest Executable Supply Machine Version ${package_json.version}`,
       "-v, --version"
     )
-    .usage("[options] [file]")
+    .usage("[options] <assembly file>")
     .argument("[file]", "assembly file to translate.")
+    .option("-h, --headerless", `Skip header generation for ROM files.`, false)
+    .option("-m, --metrics", "Show various metrics after translation", false)
+    .option("-s, --space", "Show segment and ROM bank usage.", false)
     .action((file) => {
       if (!file) {
         throw new Error("No assembly file given.");
@@ -49,7 +56,46 @@ async function main() {
   let symbols_name = program_name + "." + extensions.ndb;
   let rom_name = program_name + "." + extensions.rom;
 
+  // load assembly to be processed into memory
+  let input_assembly;
+  try {
+    input_assembly = await readFile(input_file, { encoding: "utf8" });
+  } catch (error) {
+    throw new Error(error);
+  }
+  console.log(input_assembly);
+
+  // initialize ROM
+  let rom = Array(8192 * 128).fill(0xff);
+  let map = Array(8192 * 128).fill(0xff);
+
+  // initialize instruction tables
+  let macro_instruction_set = {};
+  let function_instruction_set = {
+    ...base_instruction_set,
+    ...pseudo_instruction_set,
+  };
+  let machine_instruction_set = nes.machine_instruction_set;
+
+  // translate assembly to machine code
+
+  // write header and rom to output files
+
+  // report segment and ROM usage
+  if (program.options.space) {
+    console.log("Cartridge Segment Space Usage:");
+    console.log("Cartridge ROM Space Usage:");
+  }
+
+  // report function addresses and metrics
+  if (program.options.metrics) {
+    console.log("Cartridge ROM function addresses:");
+    console.log("Code Size:");
+    console.log("Total Instruction Calls");
+  }
+
   console.log(`${program_name} translated successfully!`);
+
   return 0;
 }
 
